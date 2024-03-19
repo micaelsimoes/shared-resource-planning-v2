ebb98a341932dfc9c10b8da21e5d6a2bdc1d6caa
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-03-19 15:50:38 +0000

    _write_shared_energy_storages_results_to_excel(). Update.

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def shared_resources_planning(working_directory, specification_filename):
     '''
 
     candidate_solution = planning_problem.get_initial_candidate_solution()
-    planning_problem.run_operational_planning(candidate_solution=candidate_solution, print_results=True, debug_flag=True)
+    planning_problem.run_operational_planning(candidate_solution=candidate_solution, print_results=True, debug_flag=False)
 
     #planning_problem.run_planning_problem()
 
