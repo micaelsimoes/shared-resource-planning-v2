acb3390c664cf869c213f49eaa83be2967f2966c
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-03-20 14:27:37 +0000

    CS1. Update

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def shared_resources_planning(working_directory, specification_filename):
     '''
     candidate_solution = planning_problem.get_initial_candidate_solution()
     esso_model = planning_problem.shared_ess_data.build_subproblem()
-    planning_problem.shared_ess_data.update_model_with_candidate_solution(esso_model, candidate_solution['total_capacity'])
+    planning_problem.shared_ess_data.update_model_with_candidate_solution(esso_model, candidate_solution['investment'])
     results = planning_problem.shared_ess_data.optimize(esso_model)
     processed_results = planning_problem.shared_ess_data.process_results(esso_model)
     planning_problem.shared_ess_data.write_optimization_results_to_excel(processed_results)
