bf2cd9995cc36f277d8190cda169c81520437463
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-03-20 14:50:47 +0000

    CS1. Update

--- shared_resources_planning.py
+++ shared_resources_planning.py
@@ -4453,7 +4453,7 @@ def _get_initial_candidate_solution(planning_problem):
             candidate_solution['investment'][node_id][year] = dict()
             candidate_solution['investment'][node_id][year]['s'] = 0.00
             candidate_solution['investment'][node_id][year]['e'] = 0.00
-            if year == 2020:
+            if year == 2020 or year == 2040:
                 candidate_solution['investment'][node_id][year]['s'] = 1.00
                 candidate_solution['investment'][node_id][year]['e'] = 1.00
             candidate_solution['total_capacity'][node_id][year] = dict()
