a9c657a1adfe67b24ecb4d4b9d3bfeafea40f818
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-03-27 17:30:52 +0000

    CS1. Update

--- main.py
+++ main.py
@@ -62,10 +62,8 @@ def shared_resources_planning(working_directory, specification_filename):
     #planning_problem.plot_diagram()
     #planning_problem.run_planning_problem()
 
-    '''
     candidate_solution = planning_problem.get_initial_candidate_solution()
     planning_problem.run_operational_planning(candidate_solution=candidate_solution, print_results=True, debug_flag=False)
-    '''
 
     '''
     transmission_network = planning_problem.transmission_network
@@ -89,11 +87,13 @@ def shared_resources_planning(working_directory, specification_filename):
         distribution_network.write_optimization_results_to_excel(processed_results)
     '''
 
+    '''
     candidate_solution = planning_problem.get_initial_candidate_solution()
     esso_model = planning_problem.shared_ess_data.build_subproblem()
     planning_problem.shared_ess_data.update_model_with_candidate_solution(esso_model, candidate_solution['investment'])
     results = planning_problem.shared_ess_data.optimize(esso_model)
     planning_problem.shared_ess_data.write_optimization_results_to_excel(esso_model)
+    '''
 
     print('==========================================================================================================')
     print('                                                 END                                                      ')
