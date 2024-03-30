993c9a27b1258d88bca7b5dfa543ad564f16252d
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-03-30 11:35:41 +0000

    Debug.

--- main.py
+++ main.py
@@ -64,6 +64,7 @@ def shared_resources_planning(working_directory, specification_filename):
     candidate_solution = planning_problem.get_initial_candidate_solution()
     planning_problem.run_operational_planning(candidate_solution=candidate_solution, print_results=True, debug_flag=True)
 
+
     '''
     candidate_solution = planning_problem.get_initial_candidate_solution()
     '''
