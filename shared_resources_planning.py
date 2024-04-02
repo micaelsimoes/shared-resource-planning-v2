ce3dcb0c69b33e6d5be85f14df53ec5438cc652a
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-04-02 18:12:43 +0100

    _update_shared_energy_storage_variables. Correction.

--- shared_resources_planning.py
+++ shared_resources_planning.py
@@ -268,7 +268,7 @@ def _run_operational_planning(planning_problem, candidate_solution, debug_flag=F
         results['dso'] = update_distribution_coordination_models_and_solve(distribution_networks, dso_models,
                                                                            consensus_vars['interface']['v'],
                                                                            consensus_vars['interface']['pf']['tso'], dual_vars['pf']['dso'],
-                                                                           consensus_vars['ess']['tso'], dual_vars['ess']['dso'],
+                                                                           consensus_vars['ess']['esso'], dual_vars['ess']['dso'],
                                                                            admm_parameters, from_warm_start=from_warm_start)
 
         # 3.1 Update ADMM CONSENSUS variables
@@ -968,8 +968,8 @@ def _update_shared_energy_storage_variables(planning_problem, tso_model, dso_mod
         for year in planning_problem.years:
             for day in planning_problem.days:
                 for t in range(planning_problem.num_instants):
-                    error_p_tso = shared_ess_vars['tso'][node_id][year][day]['p'][t] - shared_ess_vars['dso'][node_id][year][day]['p'][p]
-                    error_p_dso = shared_ess_vars['dso'][node_id][year][day]['p'][t] - shared_ess_vars['tso'][node_id][year][day]['p'][p]
+                    error_p_tso = shared_ess_vars['tso'][node_id][year][day]['p'][t] - shared_ess_vars['esso'][node_id][year][day]['p'][p]
+                    error_p_dso = shared_ess_vars['dso'][node_id][year][day]['p'][t] - shared_ess_vars['esso'][node_id][year][day]['p'][p]
                     error_p_esso_tso = shared_ess_vars['esso'][node_id][year][day]['p'][p] - shared_ess_vars['tso'][node_id][year][day]['p'][t]
                     error_p_esso_dso = shared_ess_vars['esso'][node_id][year][day]['p'][p] - shared_ess_vars['dso'][node_id][year][day]['p'][t]
                     error_p_esso_prev = shared_ess_vars['esso'][node_id][year][day]['p'][p] - shared_ess_prev_vars['esso'][node_id][year][day]['p'][t]
