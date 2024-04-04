f75d89d871202d7e76636209d73718895a19ac3f
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-04-04 11:48:02 +0100

    CS1. Update.

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def shared_resources_planning(working_directory, specification_filename):
     #planning_problem.plot_diagram()
 
     candidate_solution = planning_problem.get_initial_candidate_solution()
-    planning_problem.run_operational_planning(candidate_solution=candidate_solution, print_results=True, debug_flag=False)
+    #planning_problem.run_operational_planning(candidate_solution=candidate_solution, print_results=True, debug_flag=False)
     #planning_problem.run_without_coordination(print_results=True)
 
     '''
@@ -73,7 +73,6 @@ def shared_resources_planning(working_directory, specification_filename):
     processed_results = transmission_network.process_results(tn_model, results)
     transmission_network.write_optimization_results_to_excel(processed_results)
     '''
-    '''
     distribution_networks = planning_problem.distribution_networks
     for node_id in distribution_networks:
         distribution_network = distribution_networks[node_id]
@@ -82,7 +81,6 @@ def shared_resources_planning(working_directory, specification_filename):
         results = distribution_network.optimize(dn_model)
         processed_results = distribution_network.process_results(dn_model, results)
         distribution_network.write_optimization_results_to_excel(processed_results)
-    '''
     '''
     candidate_solution = planning_problem.get_initial_candidate_solution()
     esso_model = planning_problem.shared_ess_data.build_subproblem()
