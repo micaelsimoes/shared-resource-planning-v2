057d8f3913d0aa94cb648b46c8f3230d86678d34
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-04-05 11:56:57 +0100

    Energy storage. Parameters updated.

--- energy_storage.py
+++ energy_storage.py
@@ -4,14 +4,14 @@
 class EnergyStorage:
 
     def __init__(self):
-        self.es_id = -1           # bus number (positive integer)
+        self.es_id = -1         # bus number (positive integer)
         self.bus = -1           # bus number (positive integer)
         self.s = 0.0            # Apparent power, [MVA]
         self.e = 0.0            # Capacity (energy), [MVAh]
         self.e_init = 0.0       # Initial energy stored, [MVAh]
         self.e_min = 0.0        # Minimum energy stored, [MVAh]
         self.e_max = 0.0        # Maximum energy stored, [MVAh]
-        self.eff_ch = 0.0       # Charging efficiency, [0-1]
-        self.eff_dch = 0.0      # Discharging efficiency, [0-1]
-        self.max_pf = 0.0       # Maximum power factor
-        self.min_pf = 0.0       # Minimum power factor
+        self.eff_ch = 1.00      # Charging efficiency, [0-1]
+        self.eff_dch = 0.95     # Discharging efficiency, [0-1]
+        self.max_pf = 0.80      # Maximum power factor
+        self.min_pf = -0.80     # Minimum power factor
