2fd8670b98715b8ecfdfcc984f6743ddb9c7afec
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-04-05 16:52:46 +0100

    Shared ESS. Optimize. Test.

--- shared_energy_storage_data.py
+++ shared_energy_storage_data.py
@@ -699,11 +699,13 @@ def _optimize(model, params, from_warm_start=False):
         solver.options['print_level'] = 6
         solver.options['output_file'] = 'optim_log.txt'
 
+    '''
     if params.solver == 'ipopt':
         solver.options['tol'] = params.solver_tol
         solver.options['acceptable_tol'] = params.solver_tol * 1e3
         solver.options['acceptable_iter'] = 5
         solver.options['max_iter'] = 10000
+    '''
 
     result = solver.solve(model, tee=params.verbose)
 
