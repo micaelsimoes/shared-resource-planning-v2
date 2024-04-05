fa332f0657d54d000dcb1383fafa13bb6fcf0f98
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-04-05 14:44:55 +0100

    Shared ESS results. Correction

--- shared_resources_planning.py
+++ shared_resources_planning.py
@@ -4680,12 +4680,9 @@ def _get_initial_candidate_solution(planning_problem):
             candidate_solution['investment'][node_id][year] = dict()
             candidate_solution['investment'][node_id][year]['s'] = 0.00
             candidate_solution['investment'][node_id][year]['e'] = 0.00
-            if year == 2020 or year == 2040:
-                candidate_solution['investment'][node_id][year]['s'] = 1.00
-                candidate_solution['investment'][node_id][year]['e'] = 1.00
             candidate_solution['total_capacity'][node_id][year] = dict()
-            candidate_solution['total_capacity'][node_id][year]['s'] = 1.00
-            candidate_solution['total_capacity'][node_id][year]['e'] = 1.00
+            candidate_solution['total_capacity'][node_id][year]['s'] = 0.00
+            candidate_solution['total_capacity'][node_id][year]['e'] = 0.00
     return candidate_solution
 
 
